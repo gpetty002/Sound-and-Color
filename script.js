3a84f51919dc29800d4a4c082b60b7df3d3b03cc
Glitch (glitch-blank-website) <none>
2022-04-02 04:57:52 +0000

    🏮🌅 Checkpoint ./script.js:43514141/1722 ./index.html:43514141/537 ./style.css:43514141/68

--- script.js
+++ script.js
@@ -1,14 +1,77 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
+const clueHoldTime = 1000; // how long to hold each clue's light/sound
+
 var pattern = [2, 2, 4, 3, 2, 1,2,4];
 var progress = 0;
 var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5;
 
 function startGame() {
   // intialize game variables
   progress = 0;
   gamePlaying = true;
   
-  
+  // swap the Start and Stop buttons
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
 }
+
+function stopGame() {
+  gamePlaying = false;
+  
+  // swap the buttons again
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+}
+
+function lightButton(btn) {
+  document.getElementById("button" + btn).classList.add("lit");
+}
+
+function clearButton(btn) {
+  document.getElementById("button" + btn).classList.remove("lit");
+}
+
+// Sound Synthesis Functions
+const freqMap = {
+  1: 261.6,
+  2: 329.6,
+  3: 392,
+  4: 466.2
+}
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  context.resume()
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    context.resume()
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    context.resume()
+    tonePlaying = true
+  }
+}
+function stopTone(){
+  g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+  tonePlaying = false
+}
+
+// Page Initialization
+// Init Sound Synthesizer
+var AudioContext = window.AudioContext || window.webkitAudioContext 
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
