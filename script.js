c8241fc196971b0383db0467f46f611001fdaa2e
Glitch (glitch-blank-website) <none>
2022-04-02 04:45:50 +0000

    😂☎️ Checkpoint ./style.css:43514141/833 ./index.html:43514141/15 ./script.js:43514141/230

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-console.log("Hello, world!");
+var pattern = [2, 2, 4, 3, 2, 1,2,4];
+var progress = 0;
+var gamePlaying = false;
+
+function startGame() {
+  // intialize game variables
+  progress = 0;
+  gamePlaying = true;
+  
+  
+}
